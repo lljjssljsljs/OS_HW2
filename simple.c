c0f65a8fc0472d4e8bd33425e9837ce8ca464f05
lljjssljsljs <lijingsi910720@gmail.com>
2014-02-07 18:25:12 -0500

    Finished version

--- simple.c
+++ simple.c
@@ -6,54 +6,33 @@
 #include <linux/sched.h>
 #include <linux/list.h>
 
-struct list_head *list;
+
 void list_task(struct task_struct* task,int i){
-  	struct task_struct * task_entry;
+        struct task_struct * task_entry;
+	struct list_head *list;
         int space;
-        //list_head head_children = task->children;
-	if(list_empty(&task->children)==0){
-	//if(list_empty(task)==0){
-	printk("It is not empty\n");
-	list_for_each(list,&task->children){
-	  task_entry = list_entry(list,struct task_struct, sibling);
-	  for(space=0;space<i;space++){
-		printk(" ");
+	//       list_head head_children = task->children;
+	if(task){
+	  if(i==0){
+	    printk("-- [%d] %s \n",task->pid,task->comm);
+	  }else{
+	    for(space=0;space<i;space++){
+		printk("  ");
+	    }
+	    printk("\\_ [%d] %s \n",task->pid,task->comm);
 	  }
-	  printk("\\_[%d]\n",task_entry->pid);
-	  i++;
-	  list_task(task_entry,i);
 	}
 	
-        }else{
-		printk("empty\n");
-	}
+	list_for_each(list,&task->children){
+	  task_entry = list_entry(list,struct task_struct, sibling);
+	  list_task(task_entry,i+1);
+	  }
 }
 
 int simple_init(void)
 {
-  //struct task_struct *task,*task2;
-  //struct list_head *list2;
-  //extern struct task_struct init_task = INIT_TASK(init_task);
-  //EXPORT_SYMBOL(init_task);
   printk(KERN_INFO "Loading Module\n");
   list_task(&init_task,0);
-  
-  /*for_each_process(task){
-    printk("PID is %d\n",task->pid);
-    }*/
-   printk("\n--------------------\n");
-   /*list_for_each(list, &init_task.children){
-    if(list->prev!=NULL && list->next != NULL){
-      task = list_entry(list,struct task_struct, sibling);
-      printk("PID is %d\n",task->pid);
-      list_for_each(list2, &task->children){
-	task2 = list_entry(list2,struct task_struct, sibling);
-	printk("----PID is %d\n",task2->pid);
-
-      }
-    }
-    }*/
-
   return 0;
 }
 
